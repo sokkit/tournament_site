d60c29c21ed292b1d9c46e2b03b00e574c768ba8
Stephen <stephen_okeefe@ymail.com>
2021-05-21 22:47:00 +0100

    added jinja templating

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect
 
+app = Flask(__name__)
 
-
-
+@app.route('/Home')
+def hello_world():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
