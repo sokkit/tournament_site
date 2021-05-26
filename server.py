82d306eae1b28c91e26fe842ce20532defba2b0b
sokkit <stephen_okeefe@ymail.com>
2021-05-26 18:06:57 +0100

    connect server.py to db

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import mysql.connector
+import root_password as rp
 
 app = Flask(__name__)
+db = mysql.connector.connect(host="localhost", user="root", password=rp.get_password(), database="tourn")
 
 @app.route('/Home')
 def home():
