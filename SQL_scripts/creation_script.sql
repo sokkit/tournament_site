403077e5ae5272c8ad57c27149ad4c9bd97e679f
sokkit <stephen_okeefe@ymail.com>
2021-05-26 20:02:08 +0100

    added SP to get match data

--- SQL_scripts/creation_script.sql
+++ SQL_scripts/creation_script.sql
@@ -115,6 +115,7 @@ INSERT INTO `standings` (`tournament_id`, `user_id`, `points`)
 VALUES (1, 3, 6);
 
 -- STORED PROCEDURES
+
 -- stored procedure to view standings taking tournament id as input
 DROP PROCEDURE IF EXISTS getStandings;
 DELIMITER //
@@ -130,5 +131,27 @@ BEGIN
 	users.user_id = standings.user_id
     WHERE tournaments.tournament_id = tournamentIdIn
     ORDER BY standings.points DESC;
+END //
+DELIMITER ;
+
+-- Stored procedure to view matches taking tournament id as input
+DROP PROCEDURE IF EXISTS getMatches;
+DELIMITER //
+CREATE PROCEDURE getMatches (
+	IN tournamentIdIn INT
+)
+BEGIN
+	SELECT matches.match_id, matches.match_date, users.nick as `player one`, u2.nick as `player two`, COALESCE(u3.nick, 'draw') as `winner`, matches.state
+	FROM matches
+	JOIN tournaments ON
+	tournaments.tournament_id = matches.tournament_id
+	JOIN users ON 
+	users.user_id = matches.competitor1_id
+	JOIN users u2 ON 
+	u2.user_id = matches.competitor2_id
+	LEFT OUTER JOIN users u3 ON 
+	u3.user_id = matches.match_winner_id
+    WHERE matches.tournament_id = tournamentIdIn
+    ORDER BY matches.match_date;
 END //
 DELIMITER ;
