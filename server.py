a8462b77941f6c228e1098a6c8638235f5b34e88
sokkit <stephen_okeefe@ymail.com>
2021-05-26 20:06:14 +0100

    pull match data onto server

--- server.py
+++ server.py
@@ -15,14 +15,21 @@ def tournament():
     if request.method == 'GET':
         print("fetching data", file=sys.stdout)
         try:
+            # IDEA: GET TOURNAMENT NAME
             cur = db.cursor()
+            # Get standings data
             cur.callproc('getStandings', [1])
             iterator = cur.stored_results()
             for result in iterator:
                 standings = result.fetchall()
+            # Get match data
+            cur.callproc('getMatches', [1])
+            iterator = cur.stored_results()
+            for result in iterator:
+                matches = result.fetchall()
         except Exception as e:
             print("There was an error: ", e)
-        return render_template('tournament.html', standings = standings)
+        return render_template('tournament.html', standings = standings, matches = matches)
 
 
 if __name__ == "__main__":
